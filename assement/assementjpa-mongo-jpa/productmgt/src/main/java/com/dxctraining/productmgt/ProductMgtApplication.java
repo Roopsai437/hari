603e46885444a2674d4bb0b088250e00d776fc29
roopsai <roopsaichalla1@gmail.com>
2021-04-23 18:56:27 +0530

    first commit

---
+++ assement/assementjpa-mongo-jpa/productmgt/src/main/java/com/dxctraining/productmgt/ProductMgtApplication.java
@@ -0,0 +1,53 @@
+package com.dxctraining.productmgt;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.mongodb.client.MongoClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableMongoRepositories
+public class ProductMgtApplication {
+
+	@Value("${spring.data.mongodb.database}")
+	private String databaseName;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMgtApplication.class, args);
+	}
+
+	@Bean
+	public MongoTemplate mongoTemplate(MongoClient client){
+		MongoTemplate template=new MongoTemplate(client,databaseName);
+		return template;
+	}
+	
+	@Bean
+	public CorsFilter corsFilter(){
+		UrlBasedCorsConfigurationSource src=new UrlBasedCorsConfigurationSource();
+		CorsConfiguration configuration=new CorsConfiguration();
+		configuration.setAllowCredentials(true);
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedOrigin("*");
+		configuration.addAllowedMethod("*");
+		src.registerCorsConfiguration("/**",configuration);
+		return new CorsFilter(src);
+	}
+	
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+}
